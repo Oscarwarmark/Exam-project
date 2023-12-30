c059960c7f865ea6a3462ed8125476c27979a4df
Your name hereOscarwarmark <oscar.warmark@gmail.com>
2023-12-30 23:10:05 +0100

    created product card component and smaller css fixes

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,9 +1,78 @@
-import React from "react";
+import { useEffect, useContext } from "react";
+import { MyCartContext } from "../context/CartContext";
+import Button from "@mui/material/Button";
+import "../styles/ProductCard.css";
+const ProductCard = () => {
+  const { products, setProducts, cartItems, setCartItems } =
+    useContext(MyCartContext);
 
-export const ProductCard = () => {
+  useEffect(() => {
+    async function ListOfProducts() {
+      try {
+        const response = await fetch("/api/getProducts", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          console.log(response);
+          throw new Error("Failed to fetch products");
+        }
+
+        const productsData = await response.json();
+        setProducts(productsData);
+        console.log(productsData);
+      } catch (error) {
+        console.error("Error retrieving products:", error);
+      }
+    }
+
+    ListOfProducts();
+  }, []);
+
+  const handleClick = (product) => {
+    // Check if the product is already in the cart
+    const existingCartItem = cartItems.find(
+      (item) => item.product.id === product.id
+    );
+
+    if (existingCartItem) {
+      // If it exists, update the quantity
+      const updatedCartItems = cartItems.map((item) => {
+        if (item.product.id === product.id) {
+          return { ...item, quantity: item.quantity + 1 };
+        }
+        return item;
+      });
+
+      setCartItems(updatedCartItems);
+    } else {
+      // If it doesn't exist, add a new entry with quantity 1
+      setCartItems([...cartItems, { product: product, quantity: 1 }]);
+    }
+  };
+
   return (
     <div>
-      <div></div>
+      <div>
+        <div className="main-container">
+          {products.map((product, i) => (
+            <div key={i} className="product-card">
+              <img src={`${product.images}`} alt="" />
+              <h1>{product.name}</h1>
+              <p>{`${product.default_price.unit_amount / 100} kr`}</p>
+              <Button variant="outlined" onClick={() => handleClick(product)}>
+                lägg till i kundvagn
+              </Button>
+            </div>
+          ))}
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
+
+export default ProductCard;
