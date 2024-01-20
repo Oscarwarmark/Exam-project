6cd9e462c0e15568a0241e0d65b97a3760b2685d
Your name hereOscarwarmark <oscar.warmark@gmail.com>
2024-01-20 19:33:52 +0100

    added styling to adminOrders and changed the admin navigation. made it responsive

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,20 @@
 import { IconButton } from "@mui/material";
+import { NavLink, useLocation } from "react-router-dom";
 import "../styles/Footer.css";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 
 function Footer() {
+  const location = useLocation();
+
+  // Check if the current route is the admin page
+  const isAdminPage = location.pathname.startsWith("/admin");
+
+  if (isAdminPage) {
+    // If it's the admin page, don't render the header
+    return null;
+  }
   return (
     <div className="footer-container">
       <div>FOLLOW</div>
