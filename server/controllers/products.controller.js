f1fd0f6f8d2e41aeb82d6cd9576bfae03b043a98
Your name hereOscarwarmark <oscar.warmark@gmail.com>
2024-01-21 22:35:52 +0100

    created functions to create a new product from the admin dashboard

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -12,7 +12,7 @@ const getProductById = async (req, res) => {
     });
     res.status(200).json(product);
   } catch (error) {
-    // Handle errors appropriately
+    // Handle errors
     console.error("Error retrieving product from Stripe:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
@@ -31,26 +31,29 @@ const getProducts = async (req, res) => {
   }
 };
 
+// Create a product on Stripe
 const newProduct = async (req, res) => {
-  const { name, description, price } = req.body;
+  const { name, description, price, imageUrl } = req.body;
 
   try {
     const product = await stripe.products.create({
       name,
       description,
+      images: [imageUrl],
+      default_price_data: {
+        currency: "sek",
+        tax_behavior: "inclusive",
+        unit_amount: price * 100,
+      },
     });
 
-    const priceObj = await stripe.prices.create({
-      unit_amount: price * 100, // Stripe prices are in cents
-      currency: "usd", // Change to your preferred currency
-      product: product.id,
-    });
+    // Fetch the product to check if it has been created successfully
+    const fetchedProduct = await stripe.products.retrieve(product.id);
 
-    res.json({ productId: product.id, priceId: priceObj.id });
+    res.json({ productId: fetchedProduct.id });
   } catch (error) {
     console.error("Error creating product:", error);
     res.status(500).send("Internal Server Error");
   }
 };
-
 module.exports = { getProducts, getProductById, newProduct };
--- server/routes/products.route.js
+++ server/routes/products.route.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getProducts,
   getProductById,
+  newProduct,
 } = require("../controllers/products.controller");
 
 router.get("/", getProducts);
 router.get("/:productId", getProductById);
+router.post("/create-product", newProduct);
 
 module.exports = router;
